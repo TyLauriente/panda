0e3cd4ff44ab3a2191a8261d0fffb9e55a986287
Ty Lauriente <tlauriente@gmail.com>
2023-01-05 20:10:43 -0800

    Increase max_steer

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -1,8 +1,8 @@
 const SteeringLimits TOYOTA_STEERING_LIMITS = {
-  .max_steer = 1500,
+  .max_steer = 3000,
   .max_rate_up = 15,          // ramp up slow
   .max_rate_down = 25,        // ramp down fast
-  .max_torque_error = 350,    // max torque cmd in excess of motor torque
+  .max_torque_error = 2500,    // max torque cmd in excess of motor torque
   .max_rt_delta = 450,        // the real time limit is 1800/sec, a 20% buffer
   .max_rt_interval = 250000,
   .type = TorqueMotorLimited,
